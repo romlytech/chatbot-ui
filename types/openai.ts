2c1cd2aeb5f9b8deffecd0e988234869267d2943
Mike Roberts <mike@filtersource.com>
2024-09-20 11:09:52 -0400

    update deps and models

--- types/openai.ts
+++ types/openai.ts
@@ -6,23 +6,23 @@ export interface OpenAIModel {
 }
 
 export enum OpenAIModelID {
-  GPT_3_5 = 'gpt-3.5-turbo',
-  GPT_4 = 'gpt-4',
+  GPT_4o_mini = 'gpt-4o-mini',
+  GPT_4o = 'gpt-4o',
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
 export const fallbackModelID = OpenAIModelID.GPT_3_5;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
-  [OpenAIModelID.GPT_3_5]: {
-    id: OpenAIModelID.GPT_3_5,
-    name: 'GPT-3.5',
-    maxLength: 12000,
-    tokenLimit: 4000,
+  [OpenAIModelID.GPT_4o_mini]: {
+    id: OpenAIModelID.GPT_4o_mini,
+    name: 'GPT-4o-mini',
+    maxLength: 24000,
+    tokenLimit: 8000,
   },
-  [OpenAIModelID.GPT_4]: {
-    id: OpenAIModelID.GPT_4,
-    name: 'GPT-4',
+  [OpenAIModelID.GPT_4o]: {
+    id: OpenAIModelID.GPT_4o,
+    name: 'GPT-4o',
     maxLength: 24000,
     tokenLimit: 8000,
   },
